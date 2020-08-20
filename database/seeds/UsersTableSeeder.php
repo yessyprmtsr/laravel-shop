839a7cfd3c6d3bde3f19b21436c5423a2a297ba9
yessy <yessypermatasari8@gmail.com>
2020-08-20 15:33:09 +0700

    add login and auth

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adminUser = [
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin'),
+        ];
+        if(!User::where('email',$adminUser['email'])->exist()){
+            User::create($adminUser);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(
-	['namespace' => 'Admin', 'prefix' => 'admin'],
+	['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth']],
 	function () {
         Route::get('dashboard', 'DashboardController@index');
         Route::resource('categories', 'CategoryController');
     }
     );
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
