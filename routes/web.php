4ceac2983147143fa412b46896b4d11f2d0a1de2
yessy <yessypermatasari8@gmail.com>
2020-09-01 09:29:45 +0700

    add templating for user customer page dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 Route::group(
 	['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth']],
 	function () {
